b644a1fd4c31e490d2621ce2b97b864d6a83ae31
avireet <123054965+avireet@users.noreply.github.com>
2024-10-24 13:56:42 +0530

    exercise7

--- Exercise_7/app.js
+++ Exercise_7/app.js
@@ -3,23 +3,20 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-// Simulate user login status
-let isLoggedIn = true; // Change to true to simulate being logged in
-const username = 'John Doe'; // Simulated username
+let isLoggedIn = true; 
+const username = 'John Doe';
 
 app.get('/', (req, res) => {
     res.render('index', { isLoggedIn, username });
 });
 
-// Simulated login route
 app.get('/login', (req, res) => {
-    isLoggedIn = true; // Set to true to simulate logging in
+    isLoggedIn = true;
     res.redirect('/');
 });
 
-// Simulated logout route
 app.get('/logout', (req, res) => {
-    isLoggedIn = false; // Set to false to simulate logging out
+    isLoggedIn = false; 
     res.redirect('/');
 });
 
